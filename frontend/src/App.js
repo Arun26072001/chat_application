3fbb4e91564e3ab0eea091f8d7655343397d667b
Arunkumar AD <arun@webnexs.in>
2025-08-26 22:12:46 +0530

    updated frontend code

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3030");
+
+function App() {
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
